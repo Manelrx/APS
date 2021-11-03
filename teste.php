2e99956431fbd6478462dd3292c733055c69b7e9
Emanuel Manelrx <83252512+Manelrx@users.noreply.github.com>
2021-11-03 17:00:13 -0300

    teste

--- teste.php
+++ teste.php
@@ -1,5 +1,19 @@
 <?php
 
-$arquivo = file('metadados_fotos_APS_20212.csv');
+$arquivo = fopen('metadados_fotos_APS_20212.csv', 'r');
+$header = fgetcsv($arquivo, 1000, ",");
 
-print_r($arquivo);
+$teste = array();
+
+while ($row = fgetcsv($arquivo, 1000, ",")) {
+    $nota[] = array_combine($header, $row);
+}
+
+
+
+foreach($nota as $t){
+    $teste1 = implode(",", $t);
+    array_push($teste, "$teste1 \n" );
+}
+
+echo("$teste[1] $teste[2]");
