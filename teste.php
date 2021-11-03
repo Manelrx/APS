87bb7aeaa232d4795d1922100ce985e2a5d4452f
WisleyAraujo <wisleyaraujosantos@gmail.com>
2021-11-03 17:04:06 -0300

    descrição do que mudou
    se quiser colocar algo a mais e marcar alguem

--- teste.php
+++ teste.php
@@ -3,3 +3,16 @@
 $arquivo = file('metadados_fotos_APS_20212.csv');
 
 print_r($arquivo);
+
+//qualquer alteração realizada aqui vai aparecer no github desktop
+
+
+
+
+//ai o bom é só subir quando tu terminar algo grande, pq fica o historico, vou so te mostrar como sobe e como atualiza, começar no de atualizar, vou subir aqui o que eu tinha te mandado mais cedo
+
+
+// beleza :)
+
+
+// asoh nas branch fica so as suas alteações, top dms
